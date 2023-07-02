7b6fa4b286ce4561b0ae34a978cb11eba698cf6b
imretab <imre.tabajdi@gmail.com>
2023-07-02 19:58:21 +0200

    Updated bolt.js- More in description
    - Renaming json codes variable names and their values.
    - Added isLoggedAndAdmin function, which checks if the user is logged in and admin (important for inserting, updating and deleting goods).
    -Renaming a few routes from /raktar to /inventory.

--- Szop_ea_javított/bolt.js
+++ Szop_ea_javított/bolt.js
@@ -4,8 +4,8 @@ var app = express();
 var mysql = require('mysql');
 var loginCheck = require('./my_modules/login');
 var registerCheck = require('./my_modules/register');
-var raktar = require('./my_modules/raktar');
-var vasarlas = require('./my_modules/vasarlas');
+var inventory = require('./my_modules/inventory');
+var vasarlas = require('./my_modules/purchase');
 //var jsonCodes = require('./my_modules/jsonCodes');
 var connection = mysql.createConnection({
     host     : 'localhost',
@@ -27,25 +27,30 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api_docs',swaggerUI.serve,swaggerUI.setup(swaggerDoc));
 //json error codes
-const aruSikertelen = {"status":0,"msg":"Sikertelen árufeltöltés"}
-const aruSikeres = {"status":1,"msg":"Sikeres árufeltöltés"}
-const hianyos = {"status":2,"msg":"Hiányzó paraméterek"}
-const nemAdmin = {"status":2,"msg":"Nem vagy admin!"}
-const loginSikertelen = {'status':0,'msg':"Sikertelen bejelentkezés"}
-const loginSiker = {'status':1,'msg':"Sikeres bejelentkezés"}
-const registerSiker = {'status':1,'msg':"Sikeres regisztráció"}
-const registerSikertelen = {'status':0,'msg':"Sikertelen regisztráció"}
-const nemLogin = {"status":3,"msg":"Nem vagy bejelentkezve!"}
-const updateSikertelen = {"status":0,"msg":"Sikertelen update"}
-const updateSiker = {"status":1,"msg":"Sikeres update"}
-const torlesSikertelen ={"status":0,"msg":"Sikertelen törlés"}
-const torlesSikeres ={"status":1,"msg":"Sikeres törlés"}
-const vasarlasSikertelen = {"status":0,"msg":"Sikertelen vásárlás"}
-const vasarlasSikeres = {"status":1,"msg":"Sikeres vásárlás"}
+const goodsInsertFail = {"status":0,"msg":"Inserting good(s) was/were unsuccessful"}
+const goodsInsertSuccess = {"status":1,"msg":"Inserting good(s) was/were successful"}
+const incomplete = {"status":2,"msg":"Incomplete parameters"}
+const notAdmin = {"status":2,"msg":"You don't have the required privilage!"}
+const loginFail = {'status':0,'msg':"The attempt to login was unsuccessful"}
+const loginSuccess = {'status':1,'msg':"The attempt to login was successful"}
+const registerSuccess = {'status':1,'msg':"Successfully registered"}
+const registerFail = {'status':0,'msg':"Registered unsuccessfully"}
+const notLoggedIn = {"status":3,"msg":"You aren't logged in!"}
+const goodsUpdateFail = {"status":0,"msg":"Updating the selected product has failed"}
+const goodsupdateSuccess = {"status":1,"msg":"Updating the selected product has succeeded"}
+const goodsDeleteFail ={"status":0,"msg":"Deleting the selected product failed"}
+const goodsDeleteSuccess ={"status":1,"msg":"Deleting the selected product has succeeded"}
+const purchaseFail = {"status":0,"msg":"Purchasing the selected item has failed"}
+const purchaseSuccess = {"status":1,"msg":"Purchasing the selected item has succeeded"}
 var server = app.listen(8080,"127.0.0.1",function (){
   var host = server.address().address
   var port = server.address().port
 });
+async function isLoggedAndAdmin(values){
+  let islogged = await loginCheck.login(values.username,values.password);
+  let isadmin = await isAdmin(values.username);
+  return islogged && isadmin;
+}
 function isAdmin(uname){
   return new Promise((success,fail)=>connection.query("SELECT role FROM user WHERE username = ? and role = 'admin'",[uname],function(error, results){
     if(error) return fail(error);
@@ -60,118 +65,103 @@ app.get('/login/:username&:password',async function (req,res){
   var passwd = req.params.password;
   let tryLog = await loginCheck.login(name,passwd);
   if(tryLog){
-    res.json(loginSiker);
+    res.json(loginSuccess);
   }
   else{
-    res.json(loginSikertelen);
+    res.json(loginFail);
   }
-  });
+});
 app.post('/register',async function(req,res){
-    let values =  req.body;
+    let values = req.body;
     let isFilled = await registerCheck.areInputsfilled(values.teljesNev,values.username,values.password,values.email,values.role);
     if(isFilled){
         let isReg = await registerCheck.register(values.teljesNev,values.username,values.password,values.email,values.role);
         if(isReg){
-          res.json(registerSiker);
+          res.json(registerSuccess);
         }
     }
     else{
-        res.json(registerSikertelen);
+        res.json(registerFail);
     }
 });
-app.get('/raktar',function(req,res){
-    connection.query("SELECT * FROM raktar",function(error,results,fields){
+app.get('/inventory',function(req,res){
+    connection.query("SELECT * FROM inventory",function(error,results,fields){
     if(error) throw error;
     res.json(results);
 })});
-app.post('/raktar',async function(req,res){
+app.post('/inventory',async function(req,res){
   let values = req.body;
   if(values == null){
-    res.json(aruSikertelen);
+    res.json(goodsInsertFail);
     return;
   }
-  let islogged = await loginCheck.login(values.username,values.password);
-  if(!islogged){
-    res.json(nemLogin);
+  let isAdminAndLogged = await isLoggedAndAdmin(values);
+  if(!isAdminAndLogged) {
+    res.json(notAdmin);
     return;
   }
-  let isadmin = await isAdmin(values.username);
-  if(!isadmin){
-    res.json(nemAdmin);
-    return;
-  } 
-  let isFeltolt = await raktar.aruFeltolt(values.aruNev,values.ar,values.mennyiseg);
-  console.log(isFeltolt);
-  if(!isFeltolt){
-    res.json(hianyos);
+  let isInsertToInventory = await inventory.insertGoods(values.aruNev,values.ar,values.mennyiseg);
+  console.log(isInsertToInventory);
+  if(!isInsertToInventory){
+    res.json(goodsInsertFail);
     return;
   }
-  res.json(aruSikeres);   
+  res.json(goodsInsertSuccess);
   }
 );
-app.put('/raktar',async function(req,res){
+app.put('/inventory',async function(req,res){
   let values = req.body;
   if(values.id == null||values.aruNev == null||values.ar == null||values.mennyiseg == null){
-    res.json(hianyos);
+    res.json(incomplete);
     return;
   }
-  let islogged = await loginCheck.login(values.username,values.password);
-  if(!islogged){
-    res.json(nemLogin);
+  let isAdminAndLogged = await isLoggedAndAdmin(values);
+  if(!isAdminAndLogged) {
+    res.json(notAdmin);
     return;
   }
-  let isadmin = await isAdmin(values.username);
-  if(!isadmin){
-    res.json(nemAdmin);
-    return;
-  } 
-  let isUpdate = await raktar.updateRaktar(values.id,values.aruNev,values.ar,values.mennyiseg);
+  let isUpdate = await inventory.updateGoods(values.id,values.aruNev,values.ar,values.mennyiseg);
   if(!isUpdate) {
-    res.json(updateSikertelen);
+    res.json(goodsUpdateFail);
     return;
   }
-  res.json(updateSiker);
+  res.json(goodsupdateSuccess);
 });
-app.delete('/raktar/:id',async function(req,res){
+app.delete('/inventory/:id',async function(req,res){
   let values = req.body;
-  let islogged = await loginCheck.login(values.username,values.password);
-  if(!islogged){
-    res.json(nemLogin);
+  let isAdminAndLogged = await isLoggedAndAdmin(values);
+  if(!isAdminAndLogged) {
+    res.json(notAdmin);
     return;
   }
-  let isadmin = await isAdmin(values.username);
-  if(!isadmin){
-    res.json(nemAdmin);
-    return;
-  }
-  let isTorles = await raktar.torlesAru(req.params.id);
+  let isTorles = await inventory.deleteGoods(req.params.id);
   if(!isTorles){
-    res.json(torlesSikertelen);
+    res.json(goodsDeleteFail);
     return;
   }
-  res.json(torlesSikeres);
+  res.json(goodsDeleteSuccess);
 });
-app.put('/vasarlas',async function(req,res){
+app.put('/purchase',async function(req,res){
   let values = req.body;
   if(values.mennyiseg == null || values.id == null){
-    res.json(hianyos);
+    res.json(incomplete);
     return;
   }
   let islogged = await loginCheck.login(values.username,values.password);
   if(!islogged){
-    res.json(nemLogin);
+    res.json(notLoggedIn);
     return;
   }
-  let isVasar = await vasarlas.updateVasarlas(values.id,values.mennyiseg);
+  let isVasar = await vasarlas.purchase(values.id,values.mennyiseg);
   if(!isVasar){
-    res.json(vasarlasSikertelen);
+    res.json(purchaseFail);
     return;
   }
   let isTorol = await vasarlas.deleteWhereZero();
   if(!isTorol){
     return;
   }
-  res.json(vasarlasSikeres);
+  res.json(purchaseSuccess);
 });
 app.get('/php',async function(req,res){
   let phpConn =mysql.createConnection({

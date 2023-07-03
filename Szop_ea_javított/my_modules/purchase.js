4eba24098404f6af4481159afdc7d858967ae2b3
imretab <imre.tabajdi@gmail.com>
2023-07-03 22:28:17 +0200

    Updated inventory.js - More in description
    - db and parameter rename
    - Added insertToHistory function. This function insert into purchasehistory table

--- Szop_ea_javított/my_modules/purchase.js
+++ Szop_ea_javított/my_modules/purchase.js
@@ -3,11 +3,11 @@ let connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root', 
     password : '', 
-    database : 'bolt'
+    database : 'shop'
 });
 exports.deleteWhereZero = function deleteWhereZero(){
     return new Promise((success,fail) => {
-      connection.query("DELETE from inventory Where mennyiseg <= 0",async function(error,results){
+      connection.query("DELETE from inventory Where quantity <= 0",async function(error,results){
         if(error) return fail(error);
         else{
           if(results.length!=0) return success(true);
@@ -16,26 +16,24 @@ exports.deleteWhereZero = function deleteWhereZero(){
       });
     });
   }
-exports.purchase = function Purchase(id,mennyiseg){
+exports.purchase = function Purchase(id,quantity){
     return new Promise((success,fail) => {
-      if(!id || !mennyiseg){
+      if(!id || !quantity){
         return success(false);
       }
-      if(isNaN(id) || isNaN(mennyiseg)){
+      if(isNaN(id) || isNaN(quantity)){
         return success(false);
       }
       connection.query("SELECT * FROM inventory where id = ?",[id],async function(error,results){
         if(error) return fail(error);
         else{
-          if(results.length!= 0){
-            connection.query("UPDATE inventory SET mennyiseg = (mennyiseg-?) Where id = ?",[mennyiseg,id],async function(error,results){
+          if(results.length != 0){
+            connection.query("UPDATE inventory SET quantity = (quantity-?) Where id = ?",[quantity,id],async function(error,results){
               if(error){
-                console.log(vasarlasSikertelen);
                 return fail(error);
               }
               else{
                 if(results.length != 0) {
-                  console.log(vasarlasSikeres);
                   return success(true);
                 }
                 return success(false);
@@ -46,3 +44,21 @@ exports.purchase = function Purchase(id,mennyiseg){
       });
     });
   }
+exports.insertToHistroy = function insertToHistroy(userId,prodID, quantity){
+  return new Promise((success,fail)=>{
+    if(!userId || !prodID || !quantity){
+      return success(false);
+    }
+    if(isNaN(userId) || isNaN(prodID) || isNaN(quantity)){
+      return success(false);
+    }
+    connection.query("INSERT into purchasehistory(buyerID,productID,quantity) values(?,?,?)",[userId,prodID,quantity],async function (error,results){
+      if(error) return fail(error);
+      else{
+        if(results.length != 0) return success(true);
+        else return success(false);
+      }
+    });
+  })
+
+}

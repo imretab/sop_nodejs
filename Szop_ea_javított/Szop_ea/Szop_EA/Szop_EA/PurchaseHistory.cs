9115d3253ebf4ff36281822515e61e1bf3d124fa
imretab <imre.tabajdi@gmail.com>
2023-07-04 18:52:03 +0200

    Created purchasehistory.cs - Added schema

---
+++ Szop_ea_javított/Szop_ea/Szop_EA/Szop_EA/PurchaseHistory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+namespace Szop_EA
+{
+    class PurchaseHistory
+    {
+        [JsonPropertyName("id")]
+        public int ID { get; set; }
+        [JsonPropertyName("Buyer")]
+        public string Buyer { get; set; }
+        [JsonPropertyName("ProductName")]
+        public string Product { get; set; }
+        [JsonPropertyName("quantity")]
+        public int Quantity { get; set; }
+    }
+}

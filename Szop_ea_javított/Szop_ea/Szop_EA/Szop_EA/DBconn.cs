a5c3d69afdb109781091b93ce5592875d693c541
imretab <imre.tabajdi@gmail.com>
2023-07-07 18:03:42 +0200

    Created DBconnection class with custom exception

---
+++ Szop_ea_javított/Szop_ea/Szop_EA/Szop_EA/DBconn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using RestSharp;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szop_EA
+{
+    static class DBconn
+    {
+        public static bool Check(RestClient Client)
+        {
+            //for DB conn
+            string connectionRoute = "";
+            RestRequest checkConnection = new RestRequest(connectionRoute, Method.Get);
+            RestResponse connectionRestResponse = Client.Execute(checkConnection);
+            Response connectionResponse = Client.Deserialize<Response>(connectionRestResponse).Data;
+            if (connectionResponse is null)
+            {
+                throw new DBconnectionException();
+            }
+            if (connectionResponse.Status == 500)
+            {
+                MessageBox.Show(connectionResponse.Msg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Szop_ea_javított/Szop_ea/Szop_EA/Szop_EA/DBconnectionException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szop_EA
+{
+    class DBconnectionException:Exception
+    {
+        public DBconnectionException():base("Conncetion to server failed")
+        {
+
+        }
+    }
+}
